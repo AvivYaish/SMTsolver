0ec4e1d2a44b364fbeede1c4f832ca0ffdaea2ba
Aviv <aviv.yaish@mail.huji.ac.il>
2020-02-17 18:44:34 +0200

    Refactored all functions, cleaned them up.

--- sat_solver.py
+++ sat_solver.py
@@ -1,7 +1,11 @@
 
 
-# @staticmethod
-def find_closing_bracket(text):
+def find_closing_bracket(text: str) -> int:
+    """
+    Examples:
+
+    :return: the index of t
+    """
     flag = False
     counter = 0
     for idx, char in enumerate(text):
@@ -10,98 +14,97 @@ def find_closing_bracket(text):
             counter += 1
         elif char == ")":
             counter -= 1
-
         if flag and (counter == 0):
             return idx + 1
+    return -1
 
 
-def remove_brackets(text):
+def prepare_formula(formula: str) -> str:
+    """
+    Prepares a string formula for parsing:
+    1. "Unifies" all adjacent whitespace to a single space.
+    2. Strips leading and trailing whitespace.
+    3. Removes leading and trailing brackets.
+    Examples:
+    >>> prepare_formula('         ')
+    ''
+    >>> prepare_formula('   and    a     b    ')
+    'and a b'
+    >>> prepare_formula('   (   and a b     )     ')
+    'and a b'
+    >>> prepare_formula('(and (a) (b))')
+    'and (a) (b)'
+    >>> prepare_formula('and (a) (b)')
+    'and (a) (b)'
+    :param formula: a string representation of a formula, in SMT-LIBv2 language.
+    :return: a "cleaned" up formula, ready for parsing.
+    """
+    formula = ' '.join(formula.split()).strip()
+    if formula and (formula[0] == "(") and (find_closing_bracket(formula) == len(formula)):
+        return formula[1:-1].strip()
+    return formula
+
+
+def tseitin_tranform(formula: str):
     """
     Examples:
-    >>> remove_brackets('         ')
-    ''
-    >>> remove_brackets('   and    a     b    ')
-    'and a b'
-    >>> remove_brackets('   (   and a b     )     ')
-    'and a b'
-    >>> remove_brackets('(and (a) (b))')
-    'and (a) (b)'
-    >>> remove_brackets('and (a) (b)')
-    'and (a) (b)'
+    >>> tseitin_tranform("not (=> (not (and p q)) (not r))")
+    ({'not (=> (not (and p q)) (not r))': 0, '=> (not (and p q)) (not r)': 1, 'not (and p q)': 2, 'not r': 3, 'r': 4, 'and p q': 5, 'p': 6, 'q': 7}, {0: -1, 1: [-2, 3], 3: -4, 2: -5, 5: [[6], [7]]})
     """
-    text = ' '.join(text.split()).strip()
-    if text and (text[0] == "(") and (find_closing_bracket(text) == len(text)):
-        return text[1:-1].strip()
-    return text
-
-
-# @staticmethod
-def _tseitin_tranform(all_text="not (=> (not (and p q)) (not r))"):
-    count = 0
-    variables = {}
-    tokens = {}
-    texts_list = [all_text]
-    while texts_list:
-        cur_text = remove_brackets(texts_list.pop())
-        if not cur_text:
+    # TODO: this both parses and does the transform, should split to 2 functions
+    formula_list = [formula]
+    subformulas = {}
+    transformed_subformulas = {}
+    while formula_list:
+        cur_formula = prepare_formula(formula_list.pop())
+        if not cur_formula:
             continue
 
-        if cur_text not in tokens:
-            tokens[cur_text] = count
-            count += 1
+        if cur_formula not in subformulas:
+            subformulas[cur_formula] = len(subformulas)
 
-        split_cur_text = cur_text.split(None, 1)
+        split_cur_formula = cur_formula.split(None, 1)
 
         # Base case, only one variable
-        if len(split_cur_text) == 1:
+        if len(split_cur_formula) == 1:
             continue
 
-        op = split_cur_text[0]
-        right = split_cur_text[1]
-        if op not in {"not", "and", "or", "=>"}:
+        operator = split_cur_formula[0]
+        right_side = split_cur_formula[1]
+        if operator not in {"not", "and", "or", "=>"}:
             continue
 
-        right = remove_brackets(right)
-        if op == "not":
-            if right not in tokens:
-                tokens[right] = count
-                count += 1
+        right_side = prepare_formula(right_side)
+        if operator == "not":
+            if right_side not in subformulas:
+                subformulas[right_side] = len(subformulas)
 
-            variables[tokens[cur_text]] = -tokens[right]
-            texts_list.append(right)
+            transformed_subformulas[subformulas[cur_formula]] = -subformulas[right_side]
+            formula_list.append(right_side)
             continue
 
         # Boolean operator
-        if right and (right[0] == "("):
-            closing_idx = find_closing_bracket(right)
-            left = remove_brackets(right[:closing_idx])
-            right = remove_brackets(right[closing_idx:])
+        if right_side and (right_side[0] == "("):
+            closing_idx = find_closing_bracket(right_side)
+            left_side = prepare_formula(right_side[:closing_idx])
+            right_side = prepare_formula(right_side[closing_idx:])
         else:
-            left, right = right.split()
+            left_side, right_side = right_side.split()
+        formula_list.append(left_side)
+        formula_list.append(right_side)
 
-        texts_list.append(left)
-        texts_list.append(right)
+        if left_side not in subformulas:
+            subformulas[left_side] = len(subformulas)
+        if right_side not in subformulas:
+            subformulas[right_side] = len(subformulas)
 
-        if left not in tokens:
-            tokens[left] = count
-            count += 1
-        if right not in tokens:
-            tokens[right] = count
-            count += 1
-
-        if op == "and":
-            variables[tokens[cur_text]] = [[tokens[left]], [tokens[right]]]
-        elif op == "or":
-            variables[tokens[cur_text]] = [tokens[left], tokens[right]]
-        elif op == "=>":
-            variables[tokens[cur_text]] = [-tokens[left], tokens[right]]
-    return tokens, variables
-
-
-if __name__ == "__main__":
-    tokens, variables = _tseitin_tranform()
-    print(tokens)
-    print(variables)
+        if operator == "and":
+            transformed_subformulas[subformulas[cur_formula]] = [[subformulas[left_side]], [subformulas[right_side]]]
+        elif operator == "or":
+            transformed_subformulas[subformulas[cur_formula]] = [subformulas[left_side], subformulas[right_side]]
+        elif operator == "=>":
+            transformed_subformulas[subformulas[cur_formula]] = [-subformulas[left_side], subformulas[right_side]]
+    return subformulas, transformed_subformulas
 
 
 class sat_solver:
@@ -113,7 +116,8 @@ class sat_solver:
         with open(filename, 'r') as f:
             contents = f.read()
 
-            for line in all_lines:
+            # TODO: shouldn't pass line by line, there can be multi-line expressions
+            for line in contents:
                 tokens = line[1:-1].split()
                 if tokens[0] == "declare-const":
                     self._variables[tokens[1]] = tokens[2]
