c7248ed8d46f261e812cd81283be38c605f62446
Aviv <aviv.yaish@mail.huji.ac.il>
2020-03-20 11:45:02 +0200

    Added constants for everything.

--- formula_parser/formula_parser.py
+++ formula_parser/formula_parser.py
@@ -2,10 +2,31 @@ import re
 
 
 class FormulaParser:
+    TRUE = 'true'
+    FALSE = 'false'
+
+    NOT = 'not'
+    AND = 'and'
+    OR = 'or'
+    IMPLICATION = '=>'
+    BICONDITIONAL = '<=>'
+
+    BOOLEAN_CONSTANTS = frozenset({TRUE, FALSE})
+    BOOLEAN_UNARY_OPS = frozenset({NOT})
+    BOOLEAN_BINARY_OPS = frozenset({AND, OR, IMPLICATION, BICONDITIONAL})
+    BOOLEAN_OPS = BOOLEAN_UNARY_OPS | BOOLEAN_BINARY_OPS
+
+    EQUALITY = "="
+    UF_OPS = frozenset({EQUALITY})
+    ALL_OPS = UF_OPS | BOOLEAN_OPS
+
+    _OPENING_BRACKET = '('
+    _CLOSING_BRACKET = ')'
+    _PARAMETER_SEPARATOR = ','
+
     _DECLARATION = re.compile(r'\(\s*declare-fun\s+(\w+)\s+\(([^\)]*)\)\s+(\w+)\s*\)')
     _ASSERTION = re.compile(r'\(\s*assert\s*')
     _FUNCTION_COMMA = re.compile(r'\(.*?\)|(,)')
-    _UNARY_OPS = frozenset({"not"})
 
     @staticmethod
     def _find_closing_bracket(text: str) -> int:
@@ -15,10 +36,10 @@ class FormulaParser:
         flag = False
         counter = 0
         for idx, char in enumerate(text):
-            if char == "(":
+            if char == FormulaParser._OPENING_BRACKET:
                 flag = True
                 counter += 1
-            elif char == ")":
+            elif char == FormulaParser._CLOSING_BRACKET:
                 counter -= 1
             if flag and (counter == 0):
                 return idx + 1
@@ -35,7 +56,8 @@ class FormulaParser:
         :return: a "cleaned" up formula, ready for parsing.
         """
         formula = ' '.join(formula.split()).strip()
-        while formula and (formula[0] == "(") and (FormulaParser._find_closing_bracket(formula) == len(formula)):
+        while (formula and (formula[0] == FormulaParser._OPENING_BRACKET) and 
+               (FormulaParser._find_closing_bracket(formula) == len(formula))):
             formula = formula[1:-1].strip()
         return formula
 
@@ -45,7 +67,7 @@ class FormulaParser:
         if unparsed_parameters:
             start_idx = 0
             for match in re.finditer(FormulaParser._FUNCTION_COMMA, unparsed_parameters):
-                if match.group(0) != ",":
+                if match.group(0) != FormulaParser._PARAMETER_SEPARATOR:
                     continue
                 cur_parameter = unparsed_parameters[start_idx:match.start()]
                 parameters.append(cur_parameter)
@@ -65,7 +87,7 @@ class FormulaParser:
         return None
 
     @staticmethod
-    def parse_uf(formula: str, unary_operators=_UNARY_OPS):
+    def parse_uf(formula: str, unary_operators=BOOLEAN_UNARY_OPS):
         """
         Assumes asserts and declarations are enclosed by a single ( and ).
         """
@@ -91,7 +113,7 @@ class FormulaParser:
         return signature, parsed_formulas
 
     @staticmethod
-    def parse_formula(formula: str, unary_operators=_UNARY_OPS, signature=None):
+    def parse_formula(formula: str, unary_operators=BOOLEAN_UNARY_OPS, signature=None):
         """
         :return: given a textual representation of an SMT-LIBv2 formula, returns a tuple representation of it.
         """
