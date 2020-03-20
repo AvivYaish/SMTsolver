3f09d7b26dc789518f4ac6d93ebd455ab2b27f35
Aviv <aviv.yaish@mail.huji.ac.il>
2020-03-20 12:40:00 +0200

    A first implementation of _create_boolean_abstraction.

--- smt_solver/smt_solver.py
+++ smt_solver/smt_solver.py
@@ -1,10 +1,36 @@
+from formula_parser.formula_parser import FormulaParser
 
 
 class SMTSolver:
 
     @staticmethod
-    def _create_boolean_abstraction(signature, parsed_formulas):
+    def _create_boolean_abstraction(parsed_formula, signature, abstraction=None):
         """
-        :return:
+        :param abstraction: a dictionary that will hold the abstraction. It is update in-place, so an empty
+        dictionary must be passed as an argument!
+        :return: the abstracted formula.
         """
-        pass
+        if abstraction is None:
+            abstraction = {}
+        if not parsed_formula:
+            return parsed_formula
+
+        operator = parsed_formula[0]
+        if operator not in FormulaParser.BOOLEAN_OPS:
+            # Base cases:
+            # - A constant
+            # - Only one variable
+            # - A non-boolean operator (like "=")
+            if (operator not in FormulaParser.BOOLEAN_CONSTANTS) and (parsed_formula not in abstraction):
+                # Introduce a fresh variable, if this is not a constant
+                abstraction[parsed_formula] = len(abstraction) + 1
+            return str(abstraction[parsed_formula])
+
+        left_parameter = SMTSolver._create_boolean_abstraction(parsed_formula[1], signature, abstraction)
+        if operator in FormulaParser.BOOLEAN_UNARY_OPS:
+            return operator, left_parameter
+
+        # Binary operator
+        right_parameter = SMTSolver._create_boolean_abstraction(parsed_formula[2], signature, abstraction)
+        return operator, left_parameter, right_parameter
+
--- smt_solver/tests/test_smt_solver.py
+++ smt_solver/tests/test_smt_solver.py
@@ -1,9 +1,42 @@
 import pytest
+from formula_parser.formula_parser import FormulaParser
 from smt_solver.smt_solver import SMTSolver
 
 
 class TestSMTSolver:
 
     @staticmethod
-    def test_something():
-        assert True
+    def test_create_boolean_abstraction():
+        formula = '   ((     (   and (   a   ) (   b   )   ))   )    '
+        abstraction = {}
+        signature = {}
+        parsed_formula = FormulaParser.parse_formula(formula)
+        abstracted_formula = SMTSolver._create_boolean_abstraction(parsed_formula, signature, abstraction)
+        assert abstracted_formula == ('and', '1', '2')
+        assert abstraction == {'a': 1, 'b': 2}
+
+        formula = '(((and (   =     true     false    ) (a))))'
+        abstraction = {}
+        parsed_formula = FormulaParser.parse_formula(formula)
+        abstracted_formula = SMTSolver._create_boolean_abstraction(parsed_formula, signature, abstraction)
+        assert abstracted_formula == ('and', '1', '2')
+        assert abstraction == {('=', 'true', 'false'): 1, 'a': 2}
+
+        formula = '(declare-fun f (Int Int) Bool) (assert ((and (= (not a) f ( 1 , 2 ) ) (a))))'
+        abstraction = {}
+        signature, parsed_formula = FormulaParser.parse_uf(formula)
+        abstracted_formula = SMTSolver._create_boolean_abstraction(parsed_formula.pop(), signature, abstraction)
+        assert abstracted_formula == ('and', '1', '2')
+        assert abstraction == {'a': 2, ('=', ('not', 'a'), ('f', '1', '2')): 1}
+
+        formula = ('(declare-fun f (Int) Bool) ' +
+                   '(declare-fun g (Int) Bool) '
+                   '(assert (and (and (= g(a) c) (or (not (= f(g(a)) f(c))) (= g(a) d))) (not (= c d)))')
+        abstraction = {}
+        signature, parsed_formula = FormulaParser.parse_uf(formula)
+        abstracted_formula = SMTSolver._create_boolean_abstraction(parsed_formula.pop(), signature, abstraction)
+        assert abstracted_formula == ('and', ('and', '1', ('or', ('not', '2'), '3')), ('not', '4'))
+        assert abstraction == {('=', ('f', ('g', 'a')), ('f', 'c')): 2,
+                               ('=', ('g', 'a'), 'c'): 1,
+                               ('=', ('g', 'a'), 'd'): 3,
+                               ('=', 'c', 'd'): 4}
