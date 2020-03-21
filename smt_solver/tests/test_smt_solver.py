98c692d9e433d02a25be515f450875663e9645e6
Aviv <aviv.yaish@mail.huji.ac.il>
2020-03-21 21:44:36 +0200

    1. Added tests for congruence_closure. 2. Fixed tests for create_congruence_graph.

--- smt_solver/tests/test_smt_solver.py
+++ smt_solver/tests/test_smt_solver.py
@@ -7,6 +7,25 @@ class TestSMTSolver:
 
     @staticmethod
     def test_congruence_closure():
+        formula = ('(declare-fun f (Bool Bool) Bool) ' +
+                   '(assert (= f(a, b) a)) ' +
+                   '(assert (not (= f(f(a, b), b) a)))')
+        solver = SMTSolver(formula)
+        assert not solver._congruence_closure({1: True, 3: False})
+
+        formula = ('(declare-fun f (Bool) Bool) ' +
+                   '(assert (= f(f(f(a))) a)) ' +
+                   '(assert (= f(f(f(f(f(a))))) a)) ' +
+                   '(assert (not (= f(a) a)))')
+        solver = SMTSolver(formula)
+        assert not solver._congruence_closure({1: True, 2: True, 4: False})
+
+        formula = ('(declare-fun f (Bool) Bool) ' +
+                   '(assert (= f(x) f(y))) ' +
+                   '(assert (not (= x y)))')
+        solver = SMTSolver(formula)
+        assert solver._congruence_closure({1: True, 3: False})
+
         formula = ('(declare-fun f (Bool) Bool) ' +
                    '(assert (= f(f(f(a))) a)) ' +
                    '(assert (= f(f(f(f(f(a))))) a)) ' +
