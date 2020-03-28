ba93de42fea6bbd980e5f4bde3600f6e33447b74
Aviv <aviv.yaish@mail.huji.ac.il>
2020-03-28 16:36:40 +0300

    Fixed the bug: the problem lied in the update of the current assignment (accidentally multiplied by the wrong vector)

--- linear_solver/linear_solver.py
+++ linear_solver/linear_solver.py
@@ -13,27 +13,28 @@ class LinearSolver:
         self._x_B_star = b.astype(np.float64).copy()
 
     """
-    >>> A = np.array([[1, 1, 2], [2, 0, 3], [2, 1, 3]])
-    >>> b = np.array([4, 5, 7])
-    >>> c = np.array([3, 2, 4])
-    >>> solver = LinearSolver(A, b, c)
-    >>> solver.solve()
-    10.5
+    
     """
     def solve(self):
         """
+        >>> A = np.array([[1, 1, 2], [2, 0, 3], [2, 1, 3]])
+        >>> b = np.array([4, 5, 7])
+        >>> c = np.array([3, 2, 4])
+        >>> solver = LinearSolver(A, b, c)
+        >>> solver.solve()
+        10.5
         >>> A = np.array([[3, 4], [6, 1]])
         >>> b = np.array([6, 3])
         >>> c = np.array([2, 1])
         >>> solver = LinearSolver(A, b, c)
         >>> solver.solve()
-        13/7
+        1.8571428571428572
         >>> A = np.array([[3, 2, 1, 2], [1, 1, 1, 1], [4, 3, 3, 4]])
         >>> b = np.array([225, 117, 420])
         >>> c = np.array([19, 13, 12, 17])
         >>> solver = LinearSolver(A, b, c)
         >>> solver.solve()
-        1827
+        1827.0
         """
         while True:
             result = self._single_iteration()
@@ -43,29 +44,27 @@ class LinearSolver:
 
     def _single_iteration(self):
         y = self._btran(self._B, self._c_B)
-        A_col_idx = self._choose_entering_var(self._A_N, y, self._c_N)   # col in _A_N
+        A_col_idx = self._choose_entering_var(self._A_N, y, self._c_N)
         if A_col_idx == -1:
             return np.matmul(self._c_B, self._x_B_star)
 
         d = self._ftran(self._B, self._A_N[:, A_col_idx])
-        B_col_idx, t = self._choose_leaving_var(self._x_B_star, d)    # b is a col in _B
+        B_col_idx, t = self._choose_leaving_var(self._x_B_star, d)
         if B_col_idx == -1:
             return float('inf')
 
+        # Update the matrices
         A_col = self._A_N[:, A_col_idx].copy()
         self._A_N[:, A_col_idx] = self._B[:, B_col_idx]
         self._B[:, B_col_idx] = A_col
 
-        self._x_B_star -= t * A_col
+        # Update current assignments and objective function
+        self._x_B_star -= t * d
         self._x_B_star[B_col_idx] = t
-
-        old_c_b = self._c_B[B_col_idx]
-        self._c_B[B_col_idx] = self._c_N[A_col_idx]
-        self._c_N[A_col_idx] = old_c_b
+        self._c_B[B_col_idx], self._c_N[A_col_idx] = self._c_N[A_col_idx], self._c_B[B_col_idx]
         return None
 
     @staticmethod
-    @jit
     def _first_positive_index(arr):
         # Super-fast numba implementation, as seen in:
         # https://stackoverflow.com/questions/16243955/numpy-first-occurrence-of-value-greater-than-existing-value
@@ -80,7 +79,6 @@ class LinearSolver:
         return LinearSolver._first_positive_index(c_N - np.matmul(y, A_N))
 
     @staticmethod
-    @jit
     def _minimal_positive_index(arr):
         min_idx = -1
         for idx in range(len(arr)):
