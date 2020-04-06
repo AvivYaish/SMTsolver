f7669f2f26236ddb6ff56cbf52d4791df1eedab2
Aviv <aviv.yaish@mail.huji.ac.il>
2020-04-06 21:25:00 +0300

    Reasonable number of tests.

--- smt_solver/tq_solver/tests/test_tq_solver.py
+++ smt_solver/tq_solver/tests/test_tq_solver.py
@@ -62,7 +62,7 @@ class TestTQSolver:
 
     @staticmethod
     @pytest.mark.parametrize("variable_num, equation_num, coefficient_limits",
-                             [(5, equation_num, (-5, 5)) for equation_num in list(range(1, 25)) * 50])
+                             [(5, equation_num, (-5, 5)) for equation_num in list(range(1, 25))])
     def test_random_tq_equations(variable_num: int, equation_num: int, coefficient_limits: (int, int)):
         (all_pos_z3, equations_z3), (var_dec_our_txt, equations_our_txt), equations_our = \
             TestTQSolver.generate_random_equations(variable_num, equation_num, coefficient_limits)
