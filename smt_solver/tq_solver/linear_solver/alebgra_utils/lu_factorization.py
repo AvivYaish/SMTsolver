7507e4d401da666816df1aa91ed055fce9884634
Aviv <aviv.yaish@mail.huji.ac.il>
2020-04-06 16:40:34 +0300

    Substantially improved LUFactorization code, now it uses cool numpy tricks to speed up everything.

--- smt_solver/tq_solver/linear_solver/alebgra_utils/lu_factorization.py
+++ smt_solver/tq_solver/linear_solver/alebgra_utils/lu_factorization.py
@@ -9,55 +9,25 @@ class LUFactorization:
     """
 
     @staticmethod
-    def generate_p(matrix):
+    def generate_pivot_list(matrix):
         """
         Moving rows such that each column's max value is on the diagonal ensures us the
         resulting matrix has a LU decomposition. The function will search for the max
-        value in each column and will generate the P matrix.
-        :return: a tuple: (pivot_matrix, pivot_list), where pivot_list is as a list of (row1, row2) tuples,
-        where row1 and row2 were switched at that step.
+        value in each column and will generate a list of all pivots.
+        :return: pivot_list, a list of (row1, row2) tuples, where row1 and row2 were switched at that step.
         """
-        # cur_matrix = np.copy(matrix)
-        # # If matrix is n*n, runs in O(n*n), assuming that slicing of numpy arrays is O(1)
-        # pivot_matrix, pivot_list = np.identity(np.size(matrix, 0)), []
-        # for col in np.arange(np.size(matrix, 1)):
-        #     max_row = np.argmax(np.abs(cur_matrix[col:, col])) + col
-        #     if col != max_row:
-        #         pivot_matrix[[col, max_row], :] = pivot_matrix[[max_row, col], :]
-        #         cur_matrix[[col, max_row], :] = cur_matrix[[max_row, col], :]
-        #         pivot_list.append((col, max_row))
-        col_num = np.size(matrix, 1)  # square matrix, so this is equal to the row number, too
-        cur_matrix, pivot_matrix, pivot_list = np.copy(matrix), np.identity(col_num), []
-
-        for col_idx, cur_eta_col in enumerate(np.identity(col_num)):
-            max_row = np.argmax(np.abs(cur_matrix[col_idx:, col_idx])) + col_idx
-            if col_idx != max_row:
-                pivot_matrix[[col_idx, max_row], :] = pivot_matrix[[max_row, col_idx], :]
-                cur_matrix[[col_idx, max_row], :] = cur_matrix[[max_row, col_idx], :]
-                pivot_list.append((col_idx, max_row))
-
-            for row_idx in np.arange(col_idx + 1, col_num):
-                if cur_matrix[row_idx, col_idx] != 0:
-                    cur_eta_col[row_idx] = -cur_matrix[row_idx, col_idx] / cur_matrix[col_idx, col_idx]
-                    cur_matrix[row_idx] = np.add(cur_matrix[row_idx], cur_eta_col[row_idx] * cur_matrix[col_idx])
-
-        return pivot_matrix, pivot_list
+        cur_matrix,  pivot_list = matrix.astype(np.float64), []
+        for row_idx in np.arange(np.size(matrix, 1)):
+            max_row = np.argmax(np.abs(cur_matrix[row_idx:, row_idx])) + row_idx
+            if row_idx != max_row:
+                cur_matrix[[row_idx, max_row], :] = cur_matrix[[max_row, row_idx], :]
+                pivot_list.append((row_idx, max_row))
+            cur_matrix[row_idx + 1:] += np.outer(cur_matrix[row_idx + 1:, row_idx],
+                                                 cur_matrix[row_idx] / cur_matrix[row_idx, row_idx])
+        return pivot_list
 
     @staticmethod
-    def pivot_matrix(pivot_list, matrix, reverse=False):
-        """
-        :return: the matrix, after pivoting in-place according to 'pivots'.
-        """
-        if reverse:
-            pivot_iterable = reversed(pivot_list)
-        else:
-            pivot_iterable = pivot_list
-        for row1, row2 in pivot_iterable:
-            matrix[[row1, row2], :] = matrix[[row2, row1], :]
-        return matrix
-
-    @staticmethod
-    def pivot_array(pivot_list, array, reverse=False, in_place=True):
+    def pivot_array(pivot_list, array: np.array, reverse=False, in_place=True):
         """
         :return: the array, after pivoting according to 'pivots'.
         """
@@ -78,52 +48,31 @@ class LUFactorization:
     @staticmethod
     def lu_factorization(matrix):
         """
-        :return: An iterable(L_1^-1, L_2^-1, ..., U_N, U_N-1, ...) such that
+        :return: A list [L_1^-1, L_2^-1, ..., U_N, U_N-1, ...] such that
         matrix = L_1^-1 * ... * L_N^-1 * U_N * ... U_1, and all L_i^-1, U_i are eta matrices.
         """
-        # Could probably speed this up using nifty numpy tricks,
-        # but this still achieves the best theoretical run-time
-        col_num = np.size(matrix, 1)    # square matrix, so this is equal to the row number, too
-        cur_matrix, matrices = np.copy(matrix), []
+        # Achieves the best theoretical run-time
+        row_num, cur_matrix, matrices = np.size(matrix, 0), matrix.astype(np.float64), []
 
         # Create L matrices
-        for col_idx, cur_eta_col in enumerate(np.identity(col_num)):
-            for row_idx in np.arange(col_idx + 1, col_num):
-                if cur_matrix[row_idx, col_idx] != 0:
-                    cur_eta_col[row_idx] = -cur_matrix[row_idx, col_idx] / cur_matrix[col_idx, col_idx]
-                    cur_matrix[row_idx] += cur_eta_col[row_idx] * cur_matrix[col_idx]
-            matrices.append(EtaMatrix(col_idx, cur_eta_col).invert())
+        for row_idx, cur_eta_col in enumerate(np.identity(row_num)):
+            cur_eta_col[row_idx+1:] = -cur_matrix[row_idx+1:, row_idx] / cur_matrix[row_idx, row_idx]
+            cur_matrix[row_idx+1:] += np.outer(cur_eta_col[row_idx+1:], cur_matrix[row_idx])
+            matrices.append(EtaMatrix(row_idx, cur_eta_col).invert())
 
         # Create U matrices
-        for col_idx, cur_eta_col in enumerate(reversed(cur_matrix.T)):
-            matrices.append(EtaMatrix(col_num - col_idx - 1, cur_eta_col))
+        for row_idx, cur_eta_col in enumerate(reversed(cur_matrix.T)):
+            matrices.append(EtaMatrix(row_num - row_idx - 1, cur_eta_col))
         return matrices
 
     @staticmethod
     def plu_factorization(matrix):
         """
-        :return: (pivot_matrix, pivot_list) and an iterable(L_1^-1, L_2^-1, ..., U_N, U_N-1, ...) such that
-        pivot_matrix * matrix = L_1^-1 * ... * L_N^-1 * U_N * ... U_1, and all L_i^-1, U_i are eta matrices.
+        :return: pivot_list and a list [L_1^-1, L_2^-1, ..., U_N, U_N-1, ...] such that after pivoting the
+        input matrix according to pivot_list, it is equal to = L_1^-1 * ... * L_N^-1 * U_N * ... U_1, and
+        all L_i^-1, U_i are eta matrices.
         """
-        # col_num = np.size(matrix, 1)    # square matrix, so this is equal to the row number, too
-        # cur_matrix, matrices, pivot_matrix, pivot_list = np.copy(matrix), [], np.identity(col_num), []
-        #
-        # # Create L matrices
-        # for col_idx, cur_eta_col in enumerate(np.identity(col_num)):
-        #     max_row = np.argmax(np.abs(cur_matrix[col_idx:, col_idx])) + col_idx
-        #     if col_idx != max_row:
-        #         pivot_matrix[[col_idx, max_row], :] = pivot_matrix[[max_row, col_idx], :]
-        #         cur_matrix[[col_idx, max_row], :] = cur_matrix[[max_row, col_idx], :]
-        #         pivot_list.append((col_idx, max_row))
-        #
-        #     for row_idx in np.arange(col_idx + 1, col_num):
-        #         if cur_matrix[row_idx, col_idx] != 0:
-        #             cur_eta_col[row_idx] = -cur_matrix[row_idx, col_idx] / cur_matrix[col_idx, col_idx]
-        #             cur_matrix[row_idx] = np.add(cur_matrix[row_idx], cur_eta_col[row_idx] * cur_matrix[col_idx])
-        #     matrices.append(EtaMatrix(col_idx, cur_eta_col).invert())
-        #
-        # # Create U matrices
-        # for col_idx, cur_eta_col in enumerate(reversed(cur_matrix.T)):
-        #     matrices.append(EtaMatrix(col_num - col_idx - 1, cur_eta_col))
-        pivot_matrix, pivot_list = LUFactorization.generate_p(matrix)
-        return (pivot_matrix, pivot_list), LUFactorization.lu_factorization(np.matmul(pivot_matrix, matrix))
+        pivot_list = LUFactorization.generate_pivot_list(matrix)
+        return pivot_list, LUFactorization.lu_factorization(
+            LUFactorization.pivot_array(pivot_list, matrix, in_place=False)
+        )
--- smt_solver/tq_solver/linear_solver/alebgra_utils/tests/test_lu_factorization.py
+++ smt_solver/tq_solver/linear_solver/alebgra_utils/tests/test_lu_factorization.py
@@ -6,25 +6,22 @@ class TestLUFactorization:
     MATRIX1 = np.array([[3, 1, 0], [1, 1, 0], [4, 3, 1]])
 
     @staticmethod
-    def test_generate_p():
+    def test_generate_pivot_list():
         matrix = TestLUFactorization.MATRIX1
-        pivot_matrix, pivot_list = LUFactorization.generate_p(matrix)
-        assert np.all(np.matmul(pivot_matrix, matrix) == np.array([[4, 3, 1], [3, 1, 0], [1, 1, 0]]))
+        pivot_list = LUFactorization.generate_pivot_list(matrix)
+        assert np.all(LUFactorization.pivot_array(pivot_list, matrix) == np.array([[4, 3, 1], [3, 1, 0], [1, 1, 0]]))
 
     @staticmethod
     def test_pivoting():
         matrix = np.copy(TestLUFactorization.MATRIX1)
-        pivot_matrix, pivot_list = LUFactorization.generate_p(matrix)
-        assert np.all(LUFactorization.pivot_matrix(pivot_list, matrix) == np.array([[4, 3, 1], [3, 1, 0], [1, 1, 0]]))
-        assert np.all(LUFactorization.pivot_matrix(pivot_list, matrix, reverse=True) == TestLUFactorization.MATRIX1)
+        pivot_list = LUFactorization.generate_pivot_list(matrix)
+        assert np.all(LUFactorization.pivot_array(pivot_list, matrix) == np.array([[4, 3, 1], [3, 1, 0], [1, 1, 0]]))
+        assert np.all(LUFactorization.pivot_array(pivot_list, matrix, reverse=True) == TestLUFactorization.MATRIX1)
 
     @staticmethod
-    def test_decomposition():
-        matrix = TestLUFactorization.MATRIX1
-        pivot_matrix, pivot_list = LUFactorization.generate_p(matrix)
-        pivoted_matrix = np.matmul(pivot_matrix, matrix)
-        matrices = LUFactorization.lu_decomposition(pivoted_matrix)
+    def test_plu_decomposition():
+        pivot_list, matrices = LUFactorization.plu_factorization(TestLUFactorization.MATRIX1)
         cur_matrix = np.identity(3)
         for cur_eta_matrix in matrices:
             cur_matrix = np.matmul(cur_matrix, cur_eta_matrix.get_full_matrix())
-        assert np.all(pivoted_matrix == cur_matrix)
+        assert np.all(LUFactorization.pivot_array(pivot_list, TestLUFactorization.MATRIX1) == cur_matrix)
